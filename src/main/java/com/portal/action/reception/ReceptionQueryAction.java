68d349fd8cea01f2cb5ec2e77434485c85248050
meng yue <meng.yue@navercop.com>
2016-10-24 20:06:07 +0800

    add reception info by mengyue

--- src/main/java/com/portal/action/reception/ReceptionQueryAction.java
+++ src/main/java/com/portal/action/reception/ReceptionQueryAction.java
@@ -1,44 +1,61 @@
 
 package com.portal.action.reception;
 
-import com.portal.bean.result.CustomerSimpleInfoForm;
-import com.portal.service.CustomerInfoService;
+import com.portal.bean.result.ReceptionInfoForm;
+import com.portal.common.util.JsonUtils;
+import com.portal.common.util.WebUtils;
+import com.portal.service.ReceptionInfoService;
+import java.util.List;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import net.sf.json.JSONArray;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * 接待模块查询业务
  */
 @Controller
-//@RequestMapping("/reception")
+@RequestMapping("/reception")
 public class ReceptionQueryAction {
     private final Logger logger = LoggerFactory.getLogger(ReceptionQueryAction.class);
 
     @Autowired
-    CustomerInfoService customerInfoService;
+    protected ReceptionInfoService receptionInfoService;
 
-    @RequestMapping(value = "/frist")
-    public ModelAndView fristQuery() {
-        String phone = "15041298725";
-        CustomerSimpleInfoForm from = customerInfoService.getFristQueryInfo(phone);
-        ModelAndView model = new ModelAndView();
-        model.setViewName("reception/query_frist");
-        model.addObject("from", from);
-        return model;
+    /**
+     * 初始化进入到查询页面
+     * @param request
+     * @param response
+     * @return
+     */
+    @RequestMapping(value = "/init")
+    public String receptionING(HttpServletRequest request, HttpServletResponse response) {
+        getBasePath(request, response);
+        return "reception/inquiry_record";
     }
 
-    @RequestMapping(value = "/second")
-    public ModelAndView findByName() {
-        String cid = "1";
-        CustomerSimpleInfoForm from = customerInfoService.getFristQueryInfo(cid);
-        ModelAndView model = new ModelAndView();
-        model.setViewName("reception/query_frist");
-        model.addObject("from", from);
-        return model;
+    /**
+     * 异步查询
+     * @param request
+     * @param response
+     */
+    @RequestMapping("/query")
+    public void queryReception(HttpServletRequest request, HttpServletResponse response) {
+        getBasePath(request, response);
+        String date = "20161024";
+        List<ReceptionInfoForm> info = receptionInfoService.receptionING(request, response, date);
+        logger.debug("receptionING------>> {}", info.toString());
+        // 向前端输出
+        JsonUtils.outJsonString(JSONArray.fromObject(info).toString(), response);
+    }
+
+    public void getBasePath(HttpServletRequest request, HttpServletResponse response) {
+        String basePath = WebUtils.getBasePath(request, response);
+        request.getSession().setAttribute("basePath", basePath);
     }
 
 }
