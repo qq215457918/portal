18569c2a00d063c2af6c7e8b53028c1c126f7283
meng yue <meng.yue@navercop.com>
2017-01-10 10:57:06 +0800

    modify  logout function

--- src/main/java/com/portal/action/IndexController.java
+++ src/main/java/com/portal/action/IndexController.java
@@ -9,10 +9,8 @@ import com.portal.service.EmployeeInfoService;
 import com.portal.service.ResourceService;
 import java.util.List;
 import java.util.Set;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -28,15 +26,16 @@ public class IndexController {
     @Autowired
     private EmployeeInfoService employeeService;
 
-    @RequestMapping("/index")
-    public String index(@CurrentUser EmployeeInfo loginUser, Model model, HttpServletRequest request, HttpServletResponse response) {
+    @RequestMapping("/")
+    public String index(@CurrentUser EmployeeInfo loginUser, Model model, HttpServletRequest request,
+            HttpServletResponse response) {
         Set<String> permissions = employeeService.findPermissions(loginUser.getLoginName());
         List<Resource> menus = resourceService.findMenus(permissions);
         model.addAttribute("menus", menus);
         getBasePath(request, response);
-        return "main"; 
+        return "main";
     }
-    
+
     public void getBasePath(HttpServletRequest request, HttpServletResponse response) {
         String basePath = WebUtils.getBasePath(request, response);
         request.getSession().setAttribute("basePath", basePath);
