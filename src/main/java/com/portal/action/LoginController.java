f5c64209d48986ab5f5f60f05bef66b538f326af
meng yue <meng.yue@navercop.com>
2017-03-16 09:43:59 +0800

    登陆界面，某些情况下会无法登陆，Chrome 浏览器，尝试过多个账户，确认账户密码正确。偶发。点击登录按钮后页面刷新，但仍然停留在登录界面，无错误提示

--- src/main/java/com/portal/action/LoginController.java
+++ src/main/java/com/portal/action/LoginController.java
@@ -50,7 +50,7 @@ public class LoginController {
         } else {
             token.setRememberMe(true);
             user.login(token);
-            return "";
+            return "main";
         }
     }
 }
