1a29f5390e6a23b82e60ed4d06372003d62c6f6d
CommissarXia <331864465@qq.com>
2017-03-26 19:43:07 +0800

    提交失败重试1

--- src/main/java/com/portal/bean/VisitReportInfo.java
+++ src/main/java/com/portal/bean/VisitReportInfo.java
@@ -1,7 +1,7 @@
 package com.portal.bean;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
+import java.math.BigDecimal; 
 import java.util.Date;
 
 public class VisitReportInfo implements Serializable {
